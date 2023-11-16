44f0c14713754d7c894190ee165dfb16edc372ec
Nicolas <nicolascasclew@gmail.com>
2023-11-16 13:09:53 +0000

    initial commit

---
+++ form.php
@@ -0,0 +1,15 @@
+<form action="manage_form.php" method="post" enctype="multipart/form-data"
+    style="display: flex; flex-direction:column; max-width: 200px;">
+    <label for="mail">Correo electronico: </label>
+    <input type=" text" id="mail" name="mail">
+    <label for="text">Contraseña</label>
+
+    <input type="password" id="pass" name="pass">
+    </br>
+    <input type="submit" value="Enviar">
+</form>
+
+
+<?php
+
+?>
