f6cbb3786ba46baf445b8b40b2f8a62dc03e24b1
Nicolas <nicolascasclew@gmail.com>
2023-11-16 13:27:02 +0000

    progress

--- manage_form.php
+++ manage_form.php
@@ -5,7 +5,6 @@
 $correo = $_POST["mail"];
 $password = $_POST["pass"];
 
-//echo "aqui tenemos contraseña: $password, el user: username y el mail: $correo";
 
 $dbData = array(
     "servername" => "",
@@ -26,33 +25,34 @@ if ($mysqli->connect_error) {
     die("Conexion fallida: " . $conn->connect_error);
 }
 
-
 $query = "CREATE TABLE IF NOT EXISTS  `users` ( 
         `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
         `nombre` varchar(50) NOT NULL, 
         `correo_electronico` varchar(100) NOT NULL UNIQUE KEY, 
         `contrasena_hash` varchar(255) NOT NULL );";
+
 $res = $mysqli->query($query);
+
 
 $query = "SELECT nombre, correo_electronico, contrasena_hash FROM users 
 WHERE correo_electronico = '$correo'";
+
 $result = $mysqli->query($query);
 
+
 while ($row = $result->fetch_assoc()) {
-    //echo "la rowmnba = $row";
-    //print_r($row);
-    //$id = $row['id'];
+
     $nombre = $row['nombre'];
     $mail = $row['correo_electronico'];
     $pass = $row['contrasena_hash'];
     echo "<br>";
 
-    //echo "la id es id  y el nombre $nombre y la contraseña $pass ";
     if (password_verify($password, $pass)) {
         echo "<h1 style= color:green;>CONTRASEÑA CORRECTA</h1>";
         echo "<h2 style= color:black;>Inicio de sesion autorizado</h2>";
     } else {
         echo '<h1 style=" color:red;">CONTRASEÑA INCORRECTA</h1>';
         echo "<h2 style= color:black;>Vuelva a intentarlo</h2>";
+        echo "<a href= form.php>Volver</a>";
     }
 }
